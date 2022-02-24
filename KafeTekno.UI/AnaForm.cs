cf6eea41fa7bcac337965e013d2a4342ca3f62c5
software <software@DESKTOP-8DED6EV>
2022-02-24 16:57:19 +0300

    Add project files.

---
+++ KafeTekno.UI/AnaForm.cs
@@ -0,0 +1,79 @@
+﻿using KafeTekno.DATA;
+using KafeTekno.UI.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KafeTekno.UI
+{
+    public partial class AnaForm : Form
+    {
+        KafeVeri db = new KafeVeri(); 
+        public AnaForm()
+        {
+            InitializeComponent();
+            OrnekUrunleriYukle();
+            MasalariOlustur();
+        }
+
+        private void MasalariOlustur()
+        {
+            lvwMasalar.LargeImageList = BuyukImageListesi();
+            for (int i = 1; i <= db.MasaAdet; i++)
+            {
+                ListViewItem lvi = new ListViewItem("Masa " + i);
+                lvi.ImageKey = "bos";
+                lvi.Tag = i;
+                lvwMasalar.Items.Add(lvi);
+            }
+        }
+
+        private ImageList BuyukImageListesi()
+        {
+            ImageList il = new ImageList();
+            il.ImageSize = new Size(64, 64);
+            il.Images.Add("bos", Resources.bos);
+            il.Images.Add("dolu", Resources.dolu);
+            return il;
+        }
+
+        private void OrnekUrunleriYukle()
+        {
+            db.Urunler.Add(new Urun() { UrunAd = "Kola", BirimFiyat = 7.00m });
+            db.Urunler.Add(new Urun() { UrunAd = "Ayran", BirimFiyat = 5.00m });
+        }
+
+        private Siparis SiparisBulYaDaOlustur(int masaNo)
+        {
+            Siparis siparis = db.AktifSiparisler.FirstOrDefault(x => x.MasaNo == masaNo);
+            if (siparis == null)
+            {
+                siparis = new Siparis() { MasaNo = masaNo };
+                db.AktifSiparisler.Add(siparis);
+            }
+            return siparis;
+        }
+
+        private void lvwMasalar_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ListViewItem lvi = lvwMasalar.SelectedItems[0];
+            lvi.ImageKey = "dolu";
+            int masaNo = (int)lvi.Tag;
+            Siparis siparis = SiparisBulYaDaOlustur(masaNo);
+            new SiparisForm(db, siparis).ShowDialog();
+            if (siparis.Durum != SiparisDurum.Aktif)
+                lvi.ImageKey = "bos";
+        }
+    }
+}
+
+
+            
+
+            
---
+++ KafeTekno.UI/SiparisForm.cs
@@ -0,0 +1,102 @@
+﻿using KafeTekno.DATA;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KafeTekno.UI
+{
+    public partial class SiparisForm : Form
+    {
+        private readonly KafeVeri _db;
+        private readonly Siparis _siparis;
+        private readonly BindingList<SiparisDetay> _blSiparisDetaylar;
+
+
+        public SiparisForm(KafeVeri db, Siparis siparis)
+        {
+            _db = db;
+            _siparis = siparis;
+            _blSiparisDetaylar = new BindingList<SiparisDetay>(_siparis.SiparisDetaylar);
+            InitializeComponent();
+            cboUrun.DataSource = _db.Urunler;
+            dgvUrunler.DataSource = _blSiparisDetaylar;
+            _blSiparisDetaylar.ListChanged += _blSiparisDetaylar_ListChanged;
+            MasaNoGuncelle();
+            OdemeTutariGuncelle();
+        }
+
+        private void _blSiparisDetaylar_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            OdemeTutariGuncelle();
+        }
+
+        private void OdemeTutariGuncelle()
+        {
+            lblOdemeTutari.Text = _siparis.ToplamTutarTL;
+        }
+
+        private void MasaNoGuncelle()
+        {
+            Text = $"Masa {_siparis.MasaNo:00} (Açılış Zamanı: {_siparis.AcilisZamani})";
+            lblMasaNo.Text = _siparis.MasaNo.ToString();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (cboUrun.SelectedIndex == -1) return;
+            Urun urun = (Urun)cboUrun.SelectedItem;
+            SiparisDetay sd = new SiparisDetay() { UrunAd = urun.UrunAd, BirimFiyat = urun.BirimFiyat, Adet = (int)nudAdet.Value};
+            _blSiparisDetaylar.Add(sd);
+            EkleFormunuSifirla();
+        }
+
+        private void EkleFormunuSifirla()
+        {
+            cboUrun.SelectedIndex = 0;
+            nudAdet.Value = 1;
+        }
+
+        private void dgvUrunler_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show(
+                text: "Ürünü silmek istiyor musunuz?",
+                caption: "Detay Silme Onay", 
+                buttons: MessageBoxButtons.YesNo, 
+                icon: MessageBoxIcon.Question, 
+                defaultButton: MessageBoxDefaultButton.Button2);
+
+            e.Cancel = dr == DialogResult.No;
+        }
+
+        private void btnAnaSayfa_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOdemeAl_Click(object sender, EventArgs e)
+        {
+            MasayiKapat(SiparisDurum.Odendi, _siparis.ToplamTutar());
+        }
+
+        private void btnSiparisIptal_Click(object sender, EventArgs e)
+        {
+            MasayiKapat(SiparisDurum.Iptal);
+        }
+
+        private void MasayiKapat(SiparisDurum durum, decimal odenenTutar = 0)
+        {
+            _siparis.Durum = durum;
+            _siparis.KapanisZamani = DateTime.Now;
+            _siparis.OdenenTutar = odenenTutar;
+            _db.AktifSiparisler.Remove(_siparis);
+            _db.GecmisSiparisler.Add(_siparis);
+            Close();
+        }
+    }
+}
